285b4c75df77efb8ffc28bcee643641b188db108
irt30 <irt30@DESKTOP-MSMSVK7>
2026-07-04 14:35:43 -0600

    Semana 5: Crud Producto Completo

---
+++ src/main/java/com/tienda/repository/ProductoRepository.java
@@ -0,0 +1,13 @@
+package com.tienda.repository;
+
+import com.tienda.domain.Producto;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer>{
+    
+    //Se crea una consulta derivada para recuperar los registros de productos activas...
+    public List<Producto> findByActivoTrue();
+}
---
+++ src/main/java/com/tienda/service/ProductoService.java
@@ -0,0 +1,71 @@
+package com.tienda.service;
+
+
+import com.tienda.domain.Producto;
+import com.tienda.repository.ProductoRepository;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class ProductoService {
+    //Se enlza el repositorio de producto
+    private final ProductoRepository productoRepository;
+    //Se hace uso del repositorio firebaseStorageRepository
+    private final FirebaseStorageService firebaseStorageService;
+
+    public ProductoService(ProductoRepository productoRepository, FirebaseStorageService firebaseStorageService) {
+        this.productoRepository = productoRepository;
+        this.firebaseStorageService = firebaseStorageService;
+    }
+
+    //Recupera en un ArrayList todos los registros de categoría -o sólo activos-
+    @Transactional(readOnly=true)
+    public List<Producto> getProductos(boolean activo) {
+        if (activo){ //Solo quiero las productos activas
+            return productoRepository.findByActivoTrue();
+        }
+
+        return productoRepository.findAll();
+    }
+    
+     //Recupera en un registro de categoría -si existe-
+    @Transactional(readOnly = true)
+    public Optional<Producto> getProducto(Integer idProducto) {
+        return productoRepository.findById(idProducto);
+    }
+    
+     //Si producto, trae un idCategoría... se actualiza el registro, sino se crea
+    @Transactional
+    public  void save (Producto producto, MultipartFile imagenFile) {
+        productoRepository.save(producto);
+        if (!imagenFile.isEmpty()) { //Nos pasan una imagen...
+            try {
+                String ruta = firebaseStorageService.uploadImage(
+                    imagenFile,
+                    "producto", producto.getIdProducto());
+                producto.setRutaImagen(ruta);
+                productoRepository.save(producto);
+            } catch (IOException e){
+            }
+        }
+    }
+    //Si idProducto, se elimina... si no tiene productos asociados
+    @Transactional
+    public  void delete (Integer idProducto) {
+        //Se valida que la categoría exista...
+        if (!productoRepository.existsById(idProducto)) {
+           //Se lanza una excepcion para indicarle al usuario que no se eliminó
+           throw new IllegalArgumentException("La producto con ID "+idProducto+" no existe");
+        }
+        try {
+            productoRepository.deleteById(idProducto);
+        } catch (DataIntegrityViolationException e) {
+            throw new IllegalStateException("No se puede eliminar la producto, tiene productos asociados");
+        }
+    }
+}
